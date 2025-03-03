a0f11440bf7482778dce83aa274606b3aa771e5b
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2025-03-04 00:19:42 +0700

    section book

--- src/app/home/_sections/book.jsx
+++ src/app/home/_sections/book.jsx
@@ -1,25 +1,44 @@
+import Image from "next/image";
+
 export default function Book() {
   return (
     <section id="book" className="lg:pt-25 pt-15">
-        <div className="container">
-          <div className="lg:w-[75%] mx-auto text-center lg:px-12.5 py-7.5 px-8 border border-[rgba(0,0,0,0.05)] rounded-[20px] fadeInUp"
-            data-delay="0.2">
-            <h2
-              className="xl:leading-[90px] xl:text-[80px] md:leading-[60px] md:text-[50px] leading-[50px] text-[40px] text-black-100 font-normal">
-              See if DesignPro is right for you. (It totally is.)</h2>
-            <p className="md:text-xl md:leading-7 text-base mt-2">Get a guided tour through DesignPro, and find out how
-              you and your team can change the way
-              you source design, forever.</p>
-            <div className="mt-7.5">
-              <a href="#"
-                className="btn bg-primary text-secondary border-primary hover:bg-black-200 hover:text-white">Book a
-                free call</a>
-            </div>
-            <a href="#">
-              <img src="assets/images/logos/logo.png" alt="Logo" title="Logo" className="max-w-[200px] mx-auto" />
+      <div className="container">
+        <div className="relative mx-auto grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 text-center lg:text-left lg:px-12.5 py-7.5 px-8 rounded-[20px] bg-gradient-to-r from-[#00455F] to-[#00A5E3] text-white items-center overflow-hidden">
+          <Image
+            src="/assets/images/freelanx_common_assets/bg_lines_about_us.png"
+            alt="Background Shape"
+            layout="fill"
+            objectFit="cover"
+            className="absolute inset-0 opacity-80"
+          />
+
+          <div className="relative z-10 space-y-2">
+            <h2 className="text-3xl text-white font-bold">
+              Konsultasi dengan Freelanxx - Gratis!
+            </h2>
+            <p className="mt-2 text-sm md:text-base">
+              Diskusikan kebutuhan bisnis Anda dengan tim ahli kami! Kami siap
+              memberikan insight, solusi terbaik, dan strategi pengembangan yang
+              tepat untuk mewujudkan proyek Anda.
+            </p>
+          </div>
+          <div className="relative z-10 mt-5 lg:mt-0 grid grid-cols justify-center lg:justify-end gap-4">
+            <a
+              href="#"
+              className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-lg flex items-center gap-2 shadow-md"
+            >
+              <i className="ri-whatsapp-line text-xl"></i> Konsultasi via Whatsapp
+            </a>
+            <a
+              href="#"
+              className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-lg flex items-center gap-2 shadow-md"
+            >
+              <i className="ri-mail-line text-xl"></i> Konsultasi via Email
             </a>
           </div>
         </div>
-      </section>
-  )
+      </div>
+    </section>
+  );
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,24 +1,47 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="pt-12.5 pb-10">
-        <div className="container">
-          <div className="flex lg:flex-row flex-col items-center justify-between gap-y-4">
-            <div className="copyright-text">
-              <p>Copyright <Link href="/" className="text-primary">&copy;DesignPro</Link> All Rights Reserved.</p>
+    <footer className="bg-white ">
+      <div className="container mx-auto px-6 py-10">
+        <div className="grid sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-8">
+          
+          <div>
+            <h2 className="text-lg font-semibold text-gray-900">Freelanxx | IT Solutions</h2>
+            <p className="mt-2 text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ratione quisquam exercitationem vel explicabo illum perspiciatis laborum assumenda recusandae at architecto, quo voluptate est neque voluptatibus aliquam asperiores quas sapiente!
+            </p>
+            <div className="flex space-x-4 mt-4 text-gray-700">
+              <i className="ri-instagram-line text-xl"></i>
+              <i className="ri-whatsapp-line text-xl"></i>
+              <i className="ri-twitter-line text-xl"></i>
             </div>
-            <ul className="flex flex-wrap justify-center gap-x-7.5 gap-y-3">
-              <li><Link href="">Latest Project</Link></li>
-              <li><Link href="">Pricing</Link></li>
-              <li><Link href="">Contact</Link></li>
-              <li><Link href="">Privacy Policy</Link></li>
-              <li><Link href="">Terms & conditions</Link></li>
+          </div>
+
+          <div>
+            <h2 className="text-lg font-semibold text-gray-900">Beranda</h2>
+            <ul className="mt-2 space-y-2 text-gray-500">
+              <li><Link href="/">Layanan</Link></li>
+              <li><Link href="/">Portofolio</Link></li>
+              <li><Link href="/">Harga</Link></li>
+              <li><Link href="/">FAQs</Link></li>
             </ul>
           </div>
+
+          <div>
+            <h2 className="text-lg font-semibold text-gray-900">Contact</h2>
+            <ul className="mt-2 space-y-2 text-gray-500">
+              <li className="flex items-center gap-2"><i className="ri-phone-line"></i> (62) 812-2769-4379</li>
+              <li className="flex items-center gap-2"><i className="ri-mail-line"></i> freelanxx.it@gmail.com</li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div className=" mt-10 pt-4 sm:text-center md:text-end lg:text-end text-gray-400 text-sm">
+          © 2025 Freelanxx. All rights reserved.
         </div>
       </div>
     </footer>
-  )
+  );
 }
