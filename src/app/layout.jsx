5fa93ee678872165b85d2d54130ada8b6d5a0678
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2025-03-13 05:02:40 +0700

    foo:

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,13 +24,13 @@ export default function RootLayout({ children }) {
       <head>
 
         <meta property="og:title" content="Freelanxx - Jasa Pembuatan Aplikasi Web, Mobile, dan Desktop" />
-        <meta property="og:description" content="Jasa pembuatan aplikasi web, android, ios, dan desktop berkualitas tinggi untuk bisnis Anda, langsung dari tangan para ahli." />
+        <meta property="og:description" content="Jasa pembuatan aplikasi web, android, ios, dan desktop berkualitas tinggi untuk bisnis Anda, langsung dari tangan para ahli. Dirancang dengan teknologi mutakhir, keamanan tingkat tinggi, serta performa optimal untuk memastikan bisnis Anda berkembang pesat. Butuh solusi aplikasi custom yang inovatif, scalable, dan terpercaya? Percayakan proyek Anda kepada tim ahli kami dan wujudkan aplikasi impian Anda dengan kualitas terbaik!" />
         <meta property="og:image" content="https://www.freelanxx.com/assets/images/freelanx_common_assets/logo_header.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:url" content="https://www.freelanxx.com" />
         <meta property="og:type" content="website" />
-    
+
         <link rel="icon" href="https://www.freelanxx.com/assets/images/freelanx_common_assets/freelanxx.svg" />
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" />
